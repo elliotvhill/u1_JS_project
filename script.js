83ed203d166f56967130cf6ba64991fe6280ef86
Elliot <ellthrowaway2@icloud.com>
2023-05-19 15:57:22 -0400

    initial HTML framework, some CSS styling, testing JS functionality

---
+++ script.js
@@ -0,0 +1,10 @@
+/* BUTTONS */
+
+const startButton = document.querySelector('#start')
+const replayButton = document.querySelector('#replay')
+
+// testing button functionality
+const btnStart = () => console.log('start button clicked')
+startButton.addEventListener(('click'), btnStart)
+
+
