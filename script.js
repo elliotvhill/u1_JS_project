8e971816b6281fb08e7f60d59db971dc222b059a
Elliot <ellthrowaway2@icloud.com>
2023-05-22 10:56:04 -0400

    added function for game prompt text to 'scroll' like a typewriter

--- script.js
+++ script.js
@@ -4,7 +4,45 @@ const startButton = document.querySelector('#start')
 const replayButton = document.querySelector('#replay')
 
 // testing button functionality
-const btnStart = () => console.log('start button clicked')
-startButton.addEventListener(('click'), btnStart)
+// const btnStart = () => console.log('start button clicked')
+// const btnStart = () => typeWriter();
+startButton.addEventListener(('click'), typeWriter)
+
+/* VARIABLES */
+
+
+
+/* FUNCTIONS */
+
+
+
+/* --------- PSEUDOCODE & WHITEBOARDING ----------- */
+
+// EventListener for Start button click √
+
+// Render game board
+
+// Two text boxes: one at top for prompts, second at bottom as user input
+
+// Typewriter prompt text
+const gamePrompt = `This is some sample text.`;
+const speed = 50;
+let i = 0;
+function typeWriter() {
+    // let text = gamePrompt;
+    if (i < gamePrompt.length) {
+    document.querySelector('#prompts').innerHTML += gamePrompt.charAt(i);
+    i++
+    setTimeout(typeWriter, speed)
+    }
+}
+
+// Build out user input field and functionality
+    // enter key to submit
+
+// Need a 'lives' counter to track attempts
+    // If Lives reaches 0, game is over
+
+// Button to reset & replay
 
 
